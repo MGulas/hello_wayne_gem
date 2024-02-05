9513f46217b4bea995d4fd6a7a9487bd7b4854cb
MGulas <michael.gulasy@gmail.com>
2024-02-04 19:02:03 -0500

    init gem

---
+++ lib/hello_wayne.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "hello_wayne/version"
+
+module HelloWayne
+  def self.greet
+    puts "Hello Wayne"
+  end
+end
---
+++ spec/hello_wayne_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe HelloWayne do
+  it "has a version number" do
+    expect(HelloWayne::VERSION).not_to be nil
+  end
+end
